f78c685244136758503ffd78626bf6ac11aa25c0
Garrett Smith <garrettarhsmith@gmail.com>
2021-03-18 14:12:31 -0400

    03/18/2021 - 2

--- pages/about.js
+++ pages/about.js
@@ -9,7 +9,7 @@ import styled from 'styled-components'
 const Contents = styled.div`
     display: grid;
     place-items: center;
-    padding: 0 13rem 0 7rem;
+    padding: 0 5rem 0 5rem;
 `
 
 function about(props) {
@@ -19,7 +19,7 @@ function about(props) {
             <Navbar />
             <Container 
                 noMargin
-                style={{gridTemplateColumns: "2fr 1fr"}}
+                style={{gridTemplateColumns: "3fr 2fr"}}
             >
                 <Contents
                     as={motion.div}
@@ -27,7 +27,7 @@ function about(props) {
                     animate={{ opacity: 1 }}
                     exit={{ opacity: 0 }}
                 >
-                    <div>
+                    <div style={{maxWidth: 500}}>
                         <h1>ABOUT ME</h1>
                         <p>I'm an aspiring Full-Stack Web Developer, and passionate about developing applications that better the lives of others. I'm currently enrolled in V-School, a FullStack Web Development bootcamp, and I'm constantly learning new skills and improving current ones to make that possible. I also love teaching others about my craft. We are all teachers. We are all students.</p>
                     </div>
