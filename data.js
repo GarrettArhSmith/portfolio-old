77b03e0061061f47593a8f31b79773c58375f416
Garrett Smith <garrettarhsmith@gmail.com>
2021-03-22 12:05:01 -0400

    03/22/2021 - 2

--- data.js
+++ data.js
@@ -7,26 +7,26 @@ export const projects = [
     {
         id: '2',
         name: 'Student Progress Visualizations',
-        tech: []
+        tech: ['HTML', 'CSS', 'Javascript', 'React', 'Airtable API']
     },
     {
         id: '3',
         name: 'Weather App',
-        tech: []
+        tech: ['HTML', 'CSS', 'Javascript', 'React', 'Weather API']
     },
     {
         id: '4',
         name: 'Anonymous Forum',
-        tech: []
+        tech: ['HTML', 'CSS', 'Javascript', 'React', 'MongoDB', 'Express']
     },
     {
         id: '5',
         name: 'Food Ordering App',
-        tech: []
+        tech: ['HTML', 'CSS', 'Javascript', 'React', 'MongoDB', 'Express', "Token Authentication"]
     },
     {
         id: '6',
         name: 'Rock the Vote',
-        tech: []
+        tech: ['HTML', 'CSS', 'Javascript', 'React', 'MongoDB', 'Express', "Token Authentication"]
     }
 ]
