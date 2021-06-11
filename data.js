0ff6278da692fcaf65b57bc540ffba5260e395ad
Garrett Smith <garrettarhsmith@gmail.com>
2021-06-11 10:30:28 -0400

    06/11/2021 - 1

--- data.js
+++ data.js
@@ -65,6 +65,16 @@ export const projects = [
         repo: 'https://github.com/GarrettArhSmith/group-forum',
         tech: ['HTML', 'CSS', 'Javascript', 'React', 'MongoDB', 'Express'],
         thumbnail: '/anonymousforum-thumbnail.jpeg',
+        content: [
+            {
+                tag: 'p',
+                text: `The anonymous forum was the first group project I worked on at V School. I worked on it with two other developers, and we decided to build a sort of forum. The project, however, was built before the curriculum introduced authentication. So we just built out the app as an anonymous forum where anyone can edit anything, and you don't know who posted what. Though the forum lacks that crucial feature, I still learned a lot while working on this project. It was my introduction to working with git on a team, and to MongoDB and Express.`
+            },
+            {
+                tag: 'p',
+                text: `The app has full CRUD functionality. There are three categories users can make posts in. Users can also edit and delete posts. My personal favorite feature is that posts maintain their whitespace when submit, allowing for the positioning of text and the ability to make some fun ascii art.`
+            },
+        ]
     },
     {
         id: '5',
@@ -73,6 +83,20 @@ export const projects = [
         repo: 'https://github.com/GarrettArhSmith/food-ordering',
         tech: ['HTML', 'CSS', 'Javascript', 'React', 'MongoDB', 'Express', "Token Authentication"],
         thumbnail: 'https://images.pexels.com/photos/5920631/pexels-photo-5920631.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+        content: [
+            {
+                tag: 'p',
+                text: `The app is designed for use by three different kinds of users: admins (delivery business owners), restaurant owners, and customers. Admins have access to all the tools they need to run the business and manage users.`
+            },
+            {
+                tag: 'p',
+                text: `Restaurant owners can create, view and manage their restaurants. They can create different "menus" for each restaurant, and add new items to those menus. They can also see all the orders that have come in for that restaurant.`
+            },
+            {
+                tag: 'p',
+                text: `Customers can view all available restaurants, and select which items they want to add to their cart. Items from only one restaurant can be added at a time. They can view what's currently in their cart and submit the order.`
+            },
+        ]
     },
     {
         id: '6',
@@ -81,5 +105,15 @@ export const projects = [
         repo: 'https://github.com/GarrettArhSmith/rock-the-vote',
         tech: ['HTML', 'CSS', 'Javascript', 'React', 'MongoDB', 'Express', "Token Authentication"],
         thumbnail: 'https://images.pexels.com/photos/6257070/pexels-photo-6257070.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+        content: [
+            {
+                tag: 'p',
+                text: `Rock the vote is a platform for civic engagement. Users make posts, comment on those posts, and upvote and downvote both posts and comments. The platform focuses on political discussion and allows users to express their views and discuss different topics.`
+            },
+            {
+                tag: 'p',
+                text: `Though I built this as a V School assignment, I based the design and the voting system on reddit, and I think if I continued to work on the project, I would build it out as a reddit clone.`
+            },
+        ],
     }
 ]
